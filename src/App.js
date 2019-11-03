bb27f1b0317273deaeac52823a03e37c31e4e9d7
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-11-03 17:20:33 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React , { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      string: 'Hey RIck'
-    }
+     monsters: []
+    };
   }
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}));
+  };
+
   render(){
     return(
       <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {this.state.string}
-        </p>
-        <button onClick={()=> this.setState({string:'Whats Up?'})}>Some</button>
-      </header>
+        <CardList name="rick">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
     </div>
     );
   }
