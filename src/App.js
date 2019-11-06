c36c12e0cdfdd298118dc9fe0fc97533291a26c2
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-11-06 18:10:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React , { Component } from 'react';
 import './App.css';
-import {CardList} from './components/card-list/card-list.component';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-     monsters: []
+     monsters: [],
+     searchField: ''
     };
   }
   componentDidMount(){
@@ -15,11 +17,19 @@ class App extends Component {
       .then(users => this.setState({monsters: users}));
   };
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value})
+  }
+
   render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())); 
     return(
       <div className="App">
-        <CardList monsters={this.state.monsters} />
-    </div>
+        <SearchBox placeholder="Search Monsters" handleChange={this.handleChange} />
+
+          <CardList monsters={filteredMonsters} />
+      </div>
     );
   }
 }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import './card-list.style.css';
+
+import { Card } from '../card/card.component';
+import './card-list.styles.css';
 
 export const CardList = (props)=> (
     <div className="card-list">
         {props.monsters.map(monster => (
-            <h1 key={monster.id}></h1>
+            <Card key={monster.id} monster={monster} />
           ))}
     </div>
 );
