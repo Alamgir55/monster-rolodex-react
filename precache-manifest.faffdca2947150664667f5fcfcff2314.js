4d75184b237b3fd2922276265a1950b0f0336c0a
Alamgir Hossain <alamgir555hossain@gmail.com>
2019-11-07 18:29:39 -0500

    Updates

---
+++ precache-manifest.faffdca2947150664667f5fcfcff2314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e02338c621d649b662afdaccd3f174d",
+    "url": "/Alamgir55.github.io/monster-rolodex-react/index.html"
+  },
+  {
+    "revision": "cde5e505ed6bc75c6d51",
+    "url": "/Alamgir55.github.io/monster-rolodex-react/static/css/main.19b2610d.chunk.css"
+  },
+  {
+    "revision": "c4a047ebe933f223ff17",
+    "url": "/Alamgir55.github.io/monster-rolodex-react/static/js/2.b0265b97.chunk.js"
+  },
+  {
+    "revision": "cde5e505ed6bc75c6d51",
+    "url": "/Alamgir55.github.io/monster-rolodex-react/static/js/main.e4fb913b.chunk.js"
+  },
+  {
+    "revision": "e505f8cc31316c90064d",
+    "url": "/Alamgir55.github.io/monster-rolodex-react/static/js/runtime-main.88aa8ff1.js"
+  }
+]);
